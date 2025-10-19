821362d62a94fe8d41bb60202d08ba0ee0f998b9
Sofia <Sofia>
2025-10-19 16:41:06 -0500

    Initial Commit

---
+++ src/main/java/com/example/Taller_Aplicativo_CICD/TallerAplicativoCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.Taller_Aplicativo_CICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerAplicativoCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerAplicativoCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Taller_Aplicativo_CICD/TallerAplicativoCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Taller_Aplicativo_CICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerAplicativoCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
