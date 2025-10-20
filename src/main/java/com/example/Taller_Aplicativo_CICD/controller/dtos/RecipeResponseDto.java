5a71a952466790edc209457ff430082f896f54f1
Sofia <Sofia>
2025-10-20 18:01:33 -0500

    feat(recipe): Add Recipe feature with CRUD operations

---
+++ src/main/java/com/example/Taller_Aplicativo_CICD/controller/dtos/RecipeResponseDto.java
@@ -0,0 +1,12 @@
+package com.example.Taller_Aplicativo_CICD.controller.dtos;
+
+import java.util.List;
+
+public record RecipeResponseDto (
+        String id,
+        String author,
+        List<String> ingredients,
+        List<String> instructions,
+        int season
+) {
+}
---
+++ src/main/java/com/example/Taller_Aplicativo_CICD/model/repositories/RecipeRepository.java
@@ -0,0 +1,18 @@
+package com.example.Taller_Aplicativo_CICD.model.repositories;
+
+import com.example.Taller_Aplicativo_CICD.model.models.Recipe;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface RecipeRepository extends MongoRepository<Recipe, String> {
+    @Query("{author: ?0}")
+    Recipe findByAuthor(String authorName);
+
+    Optional<Recipe> findByNum(int number);
+
+    @Query("{'ingredients': {$in: [?0]}}")
+    List<Recipe> findByIngredient(String ingredient);
+}
