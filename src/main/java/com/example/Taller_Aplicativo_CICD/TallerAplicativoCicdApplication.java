f5f361ad9cfaeb5b53ddcbfb33ae8ce0e64293c3
Sofia <Sofia>
2025-10-20 20:18:31 -0500

    feat(core): Add initial application and build workflow

---
+++ src/main/java/com/example/Taller_Aplicativo_CICD/TallerAplicativoCicdApplication.java
@@ -0,0 +1,17 @@
+package com.example.Taller_Aplicativo_CICD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main Spring Boot application class.
+ * This class serves as the entry point for the application.
+ */
+@SpringBootApplication
+public class TallerAplicativoCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TallerAplicativoCicdApplication.class, args);
+    }
+
+}
